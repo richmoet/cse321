3efc769381d20f950e75c745c85720b4347e9b1b
Emily Richmond <richmoet@miamioh.edu>
2017-02-26 18:58:33 -0500

    Pushing all files

---
+++ homework2/src/homework2/Calc.java
@@ -0,0 +1,13 @@
+package homework2;
+// Introduction to Software Testing
+// Authors: Paul Ammann & Jeff Offutt
+// Chapter 3; page ??
+// See CalcTest.java, DataDrivenCalcTest.java for JUnit tests
+
+public class Calc
+{
+   static public int add (int a, int b)
+   {
+      return a + b;
+   }
+}
