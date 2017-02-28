745d33cb2986faa4a55cd60d65007f0f9dd49b40
richmoet <richmoet@miamioh.edu>
2017-02-27 23:49:05 -0500

    Add files via upload

--- Calc.java
+++ Calc.java
@@ -3,10 +3,24 @@
 // Chapter 3; page ??
 // See CalcTest.java, DataDrivenCalcTest.java for JUnit tests
 
-public class Calc
-{
-   static public int add (int a, int b)
-   {
+public class Calc {
+   public static int add(int a, int b) {
       return a + b;
+   }
+   
+   public static int subtract(int a, int b) {
+	   return Math.subtractExact(a, b);
+   }
+   
+   public static int multiply(int a, int b) {
+	   return Math.multiplyExact(a, b);
+   }
+   
+   public static int divide(int a, int b) {
+	   if (b == 0) {
+		   throw new ArithmeticException("Cannot divide by zero.");
+	   }
+	   
+	   return a / b;
    }
 }
